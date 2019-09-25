24a07d89940b5035854114b6ee087020481141cc
Evert-Aron Loorits <evert-aron.loorits@khk.ee>
2019-09-25 10:43:54 +0300

    Ülesanne 1 - võimalik lahendus

---
+++ 25.09.2019/ex1.php
@@ -0,0 +1,8 @@
+<?php
+echo '<h1>Info minust</h1>';
+echo 'Eesnimi: Evert-Aron<br>';
+echo 'Perenimi: Loorits<br>';
+echo 'Email: <a href="mailto:"evert.aron.loorits@gmail.com">evert.aron.loorits@gmail.com</a><br>';
+echo 'Vanus: 19<br>';
+echo 'Pikkus: 1.90(m)<br>';
+echo 'Kaal: 75(kg)<br>';
