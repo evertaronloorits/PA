48e65a87ffb9b088302e52666fc1466af33287b5
Evert-Aron Loorits <evert-aron.loorits@khk.ee>
2019-09-27 09:26:40 +0300

    Raadius_do

---
+++ 27.09.2019/Raadius_do.php
@@ -0,0 +1,23 @@
+<?php
+// raadius_do.php
+// defineerime muutuja $raadius ja omistame kasutaja poolt sisestatud väärtuse
+$raadius = $_GET['raadius'];
+// väljastame saadud väärtuse ekraanil
+echo 'Sisestatud raadius on '.$raadius.'<br>';
+// arvutused
+// ringi pindala
+$ringiPindala = 3.14 * $raadius * $raadius;
+// ringi ümbermõõt
+$ringiÜmbermõõt = 3.14 * 2 * $raadius;
+// muutujate andmete tüübi ja väärtuste kontroll
+var_dump($ringiPindala);
+var_dump($ringiÜmbermõõt);
+// var_dump($ringiPindala);
+// var_dump($ringiÜmbermõõt);
+// väljastame tulemused
+echo 'Ringi pindala on '.$ringiPindala.'<br>;
+echo 'Ringi ümbermõõt on '.$ringiÜmbermõõt.'<br>;
+
+
+
+
---
+++ 27.09.2019/Raadius_input.php
@@ -0,0 +1,9 @@
+<?php
+// raadius_input.php
+?>
+<form action="Raadius_do.php" method="get">
+    <label for="radius">Sisesta ringi raadius</label>
+        <input type="number" id="radius" name="raadius">
+
+</form>
+
