23a70cef33546579d5b899088bc5149f6ede715f
Evert-Aron Loorits <evert-aron.loorits@khk.ee>
2019-10-08 09:06:19 +0300

    27.09.2019 päeva teemad

---
+++ 08.10.2019/Tingimuslause.php
@@ -0,0 +1,17 @@
+<?php
+// define variables
+$number = 25;
+// if statement - if number is > than 0
+// control the value of logical statement
+var_dump(expression.$number > 0);
+if($number > 0) {
+    // work if number > than 0
+
+    echo $number . ' on positiivne';
+    if($number < 0) {
+        // work if number < than 0
+        echo $number.'on negatiivne<br>';
+    }
+}
+// the program is continue
+
