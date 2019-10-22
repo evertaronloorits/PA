03871e45a495b65945c1f98a02659c56d32596c4
Evert-Aron Loorits <evert-aron.loorits@khk.ee>
2019-10-22 10:51:45 +0300

    17.10.2019 Pi, maxCount...

--- 08.10.2019/22.10.2019/prime-number.php
+++ 08.10.2019/22.10.2019/prime-number.php
@@ -1,7 +1,6 @@
 <?php
 //define function
 function isPrimenumber($number) {
-    $result = '';
     if ($number < 2) {
         $result = $number. 'Antud arv ei kuulu vahemiku sisse';
     } else {
@@ -12,12 +11,25 @@ function isPrimenumber($number) {
         }
 //if number and divider is not equal - prime number
         if ($number == $divider) {
-            $result = $number . 'on algarv<br>';
+            $result = true;
         } else {
             //otherwise
-           $result = $number . 'ei ole algarv<br>';
+           $result = false;
         }
     }
+    return $result;
 }
-
 isPrimenumber(rand(0,1000));
+
+//define function , which use the boolean result of isPrimenumber and
+// show the text - is prime or is not prime with the number value
+function isPrimeNumberDescription($number, $isPrime) {
+    if($isPrime) {
+        echo $number . ' on algarv<br>';
+    } else {
+        echo $number. 'ei ole algarv<br>';
+    }
+}
+$number = rand(1,100);
+isPrimeNumberDescription($number, isPrimenumber($number));
+
